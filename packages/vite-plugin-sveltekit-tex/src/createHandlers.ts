132659262daaeff6b1a78766f4fbe50ba86700ea
Kelvin Soh <kelvinsjk@gmail.com>
2023-01-22 16:26:37 +0800

    bugfix for transformTex

--- packages/vite-plugin-sveltekit-tex/src/createHandlers.ts
+++ packages/vite-plugin-sveltekit-tex/src/createHandlers.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs-extra';
-import { blue, green } from 'kleur/colors';
+import { green } from 'kleur/colors';
 
 export async function createHandlers(): Promise<void> {
 	const exists: string[] = [];
@@ -16,33 +16,4 @@ export async function createHandlers(): Promise<void> {
 			exists.push(ext);
 		}
 	});
-	const texToHtmlPath = path.resolve(
-		`./src/lib/mathlified/content-handlers/mathlifiedTexToHtml.ts`,
-	);
-	const texToTexPath = path.resolve(
-		`./src/lib/mathlified/content-handlers/mathlifiedTexToTex.ts`,
-	);
-	if (fs.existsSync(texToHtmlPath)) {
-		fs.copySync(
-			path.resolve(
-				'./node_modules/vite-plugin-sveltekit-tex/dist/mathlifiedTexToHtml.ts',
-			),
-			texToHtmlPath,
-		);
-		console.log(blue(`Mathlified: texToHtml content handler generated`));
-	} else {
-		exists.push('texToHtml');
-	}
-	if (fs.existsSync(texToTexPath)) {
-		fs.copySync(
-			path.resolve('./node_modules/vite-plugin-sveltekit-tex/dist/mathlifiedTexToTex.ts'),
-			texToTexPath,
-		);
-		console.log(blue(`Mathlified: texToTex content handler generated`));
-	} else {
-		exists.push('texToTex');
-	}
-	if (exists.length !== 0) {
-		console.log(`Mathlified: ${exists} content handlers exist and are not replaced.`);
-	}
 }
--- packages/vite-plugin-sveltekit-tex/src/transformTex.ts
+++ packages/vite-plugin-sveltekit-tex/src/transformTex.ts
@@ -1,85 +1,84 @@
 export function texToHtml(tex: string): [string, Set<string>] {
 	const environments: Set<string> = new Set();
-	const data =
-		'<p>' +
-		tex
-			.replace(/(?<!\\)%(.*)/gm, () => {
-				return ``;
-			})
-			.replaceAll('\n\n', '\n\n<p>\n')
-			.replace(/(?<![\\])(?<![$])\$(?!\$)(.+)(?<![\\])(?<![$])\$(?!\$)/g, (_, text) => {
-				environments.add('math');
-				return `{@html math(\`${text}\`)}`;
-			})
-			.replace(/(?<!\\)\$\$(.+)(?<!\\)\$\$/g, (_, text) => {
-				environments.add('display');
-				return `{@html display(\`${text}\`)}\n\n<p>`;
-			})
-			.replace(/\\begin{align}(.+)\\end{align}/gs, (_, env) => {
-				environments.add('align');
-				return `{@html align(\`${env.replace('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
-			})
-			.replace(/\\begin{align\*}(.+)\\end{align\*}/gs, (_, env) => {
-				environments.add('alignStar');
-				return `{@html alignStar(\`${env.replace('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
-			})
-			.replace(/\\begin{alignat}{(.+)}(.+)\\end{alignat}{(.+)}/gs, (_, no, env) => {
-				environments.add('alignat');
-				return `{@html alignat(\`${env.replace('\\\\', '\\\\\\\\')}\`, ${no})}\n\n<p>`;
-			})
-			.replace(/\\begin{alignat\*}{(.+)}(.+)\\end{alignat\*}/gs, (_, no, env) => {
-				environments.add('alignAtStar');
-				return `{@html alignAtStar(\`${env.replace(
-					'\\\\',
-					'\\\\\\\\',
-				)}\`, ${no})}\n\n<p>`;
-			})
-			.replace(/\\begin{equation}(.+)\\end{equation}/gs, (_, env) => {
-				environments.add('equation');
-				return `{@html equation(\`${env.replace('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
-			})
-			.replace(/\\begin{equation\*}(.+)\\end{equation\*}/gs, (_, env) => {
-				environments.add('equationStar');
-				return `{@html equationStar(\`${env.replace('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
-			})
-			.replace(/\\begin{gather}(.+)\\end{gather}/gs, (_, env) => {
-				environments.add('gather');
-				return `{@html gather(\`${env.replace('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
-			})
-			.replace(/\\begin{gather\*}(.+)\\end{gather\*}/gs, (_, env) => {
-				environments.add('gatherStar');
-				return `{@html gatherStar(\`${env.replace('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
-			})
-			.replaceAll(/\\newline\s{0,}/g, '\n<br>')
-			.replaceAll(/\\linebreak\s{0,}/g, '\n<br>')
-			.replace(/\\emph{(.+?)}/g, (_, text) => {
-				return `<em>${text}</em>`;
-			})
-			.replace(/\\textbf{(.+?)}/g, (_, text) => {
-				return `<b>${text}</b>`;
-			})
-			.replace(/^# (.+)\n/gm, (_, text) => {
-				return `\\section{${text}}\n`;
-			})
-			.replace(/^## (.+)\n/gm, (_, text) => {
-				return `\\subsection{${text}}\n`;
-			})
-			.replace(/^### (.+)\n/gm, (_, text) => {
-				return `\\subsubsection{${text}}\n`;
-			});
+	let data = tex
+		.replace(/(?<!\\)%(.*)/gm, () => {
+			return ``;
+		})
+		.replaceAll('\n\n', '\n\n<p>\n')
+		.replace(/(?<![\\])(?<![$])\$(?!\$)(.+)(?<![\\])(?<![$])\$(?!\$)/g, (_, text) => {
+			environments.add('math');
+			return `{@html math(\`${text.replaceAll('\\\\', '\\\\\\\\')}\`)}`;
+		})
+		.replace(/(?<!\\)\$\$(.+)(?<!\\)\$\$/g, (_, text) => {
+			environments.add('display');
+			return `{@html display(\`${text.replaceAll('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
+		})
+		.replace(/\\begin{align}(.+?)\\end{align}/gs, (_, env) => {
+			environments.add('align');
+			return `{@html align(\`${env.replaceAll('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
+		})
+		.replace(/\\begin{align\*}(.+?)\\end{align\*}/gs, (_, env) => {
+			environments.add('alignStar');
+			return `{@html alignStar(\`${env.replaceAll('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
+		})
+		.replace(/\\begin{alignat}{(.+?)}(.+)\\end{alignat}{(.+)}/gs, (_, no, env) => {
+			environments.add('alignat');
+			return `{@html alignat(\`${env.replaceAll('\\\\', '\\\\\\\\')}\`, ${no})}\n\n<p>`;
+		})
+		.replace(/\\begin{alignat\*}{(.+?)}(.+)\\end{alignat\*}/gs, (_, no, env) => {
+			environments.add('alignAtStar');
+			return `{@html alignAtStar(\`${env.replaceAll(
+				'\\\\',
+				'\\\\\\\\',
+			)}\`, ${no})}\n\n<p>`;
+		})
+		.replace(/\\begin{equation}(.+?)\\end{equation}/gs, (_, env) => {
+			environments.add('equation');
+			return `{@html equation(\`${env.replaceAll('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
+		})
+		.replace(/\\begin{equation\*}(.+?)\\end{equation\*}/gs, (_, env) => {
+			environments.add('equationStar');
+			return `{@html equationStar(\`${env.replaceAll('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
+		})
+		.replace(/\\begin{gather}(.+?)\\end{gather}/gs, (_, env) => {
+			environments.add('gather');
+			return `{@html gather(\`${env.replace('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
+		})
+		.replace(/\\begin{gather\*}(.+?)\\end{gather\*}/gs, (_, env) => {
+			environments.add('gatherStar');
+			return `{@html gatherStar(\`${env.replace('\\\\', '\\\\\\\\')}\`)}\n\n<p>`;
+		})
+		.replaceAll(/\\newline\s{0,}/g, '\n<br>')
+		.replaceAll(/\\linebreak\s{0,}/g, '\n<br>')
+		.replace(/\\emph{(.+?)}/g, (_, text) => {
+			return `<em>${text}</em>`;
+		})
+		.replace(/\\textbf{(.+?)}/g, (_, text) => {
+			return `<b>${text}</b>`;
+		})
+		.replace(/^### (.+)\n/gm, (_, text) => {
+			return `<h3>${text}</h3>\n`;
+		})
+		.replace(/^## (.+)\n/gm, (_, text) => {
+			return `<h2>${text}</h2>\n`;
+		})
+		.replace(/^# (.+)\n/gm, (_, text) => {
+			return `<h1>${text}</h1>\n`;
+		});
+	data = data[0] === '<' ? data : '<p>' + data;
 	return [data, environments];
 }
 
 export function texToTex(tex: string): string {
 	const data = tex
-		.replace(/# (.+)\n^/gm, (_, text) => {
-			return `\\section{${text}}\n`;
+		.replace(/### (.+)\n^/gm, (_, text) => {
+			return `\\subsubsection{${text}}\n`;
 		})
 		.replace(/## (.+)\n^/gm, (_, text) => {
 			return `\\subsection{${text}}\n`;
 		})
-		.replace(/## (.+)\n^/gm, (_, text) => {
-			return `\\subsubsection{${text}}\n`;
+		.replace(/# (.+)\n^/gm, (_, text) => {
+			return `\\section{${text}}\n`;
 		});
 	return data;
 }
